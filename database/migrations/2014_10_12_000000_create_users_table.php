bce60976969469cf01d77ceae5e04c56ecf09921
Maiglon Lubacheuski <Maiglon@Maiglons-MacBook-Air.local>
2018-05-07 00:23:06 -0300

    Iniciando desenvolvimento.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('name', 100);
+            $table->string('username', 50)->nullable()->unique();
+            $table->string('email', 100)->unique();
+            $table->string('password', 100);
             $table->rememberToken();
             $table->timestamps();
         });
